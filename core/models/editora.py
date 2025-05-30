e9343c161e0ccaafa3aebacdef70d76d52d64aff
analauradias <anamanfrondias@gmail.com>
2025-05-30 15:04:12 -0300

    feat: adicionando Cloudinary

--- core/models/editora.py
+++ core/models/editora.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
-    site = models.CharField(max_length=100, null=True, blank=True)
+    site = models.CharField(max_length=1000, null=True, blank=True)
     email = models.EmailField(max_length=100, blank=True, null=True)
     cidade = models.CharField(max_length=100, blank=True, null=True)
 
