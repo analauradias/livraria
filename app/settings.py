4287d74e45377e16c0edbe51c0f81fdf961cf99e
analauradias <anamanfrondias@gmail.com>
2025-06-10 08:55:37 -0300

    feat: permitir apenas usuários autenticados

--- app/settings.py
+++ app/settings.py
@@ -152,6 +152,9 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'PAGE_SIZE': 10,
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticated",
+    ]
 }
 
 PASSAGE_APP_ID = os.getenv('PASSAGE_APP_ID', 'app_id')
