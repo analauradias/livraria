00647e66d60ecf358f90db9a5a47a3d2a73a30cd
analauradias <anamanfrondias@gmail.com>
2025-07-04 15:32:37 -0300

    feat: limitando os campos dos itens da compra na listagem de compras

--- core/admin.py
+++ core/admin.py
@@ -7,7 +7,8 @@ from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.utils.translation import gettext_lazy as _
 
 from core import models
-from core.models import Compra, ItensCompra
+from core.models import Autor, Categoria, Editora, Livro, Compra, ItensCompra
+
 
 
 class UserAdmin(BaseUserAdmin):
@@ -51,11 +52,48 @@ class UserAdmin(BaseUserAdmin):
         ),
     )
 
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email')
+    search_fields = ('nome', 'email')
+    list_filter = ('nome',)
+    ordering = ('nome', 'email')
+    list_per_page = 10
 
-admin.site.register(models.User, UserAdmin)
-admin.site.register(models.Categoria)
-admin.site.register(models.Editora)
-admin.site.register(models.Autor)
-admin.site.register(models.Livro)
-admin.site.register(Compra)
-admin.site.register(ItensCompra)
+@admin.register(Categoria)
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('descricao',)
+    search_fields = ('descricao',)
+    list_filter = ('descricao',)
+    ordering = ('descricao',)
+    list_per_page = 10
+
+@admin.register(Editora)
+class EditoraAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cidade')
+    search_fields = ('nome', 'email', 'cidade')
+    list_filter = ('nome', 'email', 'cidade')
+    ordering = ('nome', 'email', 'cidade')
+    list_per_page = 10
+
+@admin.register(Livro)
+class LivroAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'editora', 'categoria')
+    search_fields = ('titulo', 'editora__nome', 'categoria__descricao')
+    list_filter = ('editora', 'categoria')
+    ordering = ('titulo', 'editora', 'categoria')
+    list_per_page = 25 
+
+class ItensCompraInline(admin.TabularInline):
+    model = ItensCompra
+    extra = 1 
+
+
+@admin.register(Compra)
+class CompraAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'status')
+    search_fields = ('usuario', 'status')
+    list_filter = ('usuario', 'status')
+    ordering = ('usuario', 'status')
+    list_per_page = 10
+    inlines = [ItensCompraInline]
--- core/serializers/compra.py
+++ core/serializers/compra.py
@@ -8,12 +8,15 @@ class ItensCompraSerializer(ModelSerializer):
     class Meta:
         model = ItensCompra
         fields = ('livro', 'quantidade')
-        
+        depth = 2
+
 class CompraSerializer(ModelSerializer):
+    usuario = CharField(source='usuario.e-mail', read_only=True)
+    itens = ItensCompraSerializer(many=True, read_only=True)
+    status = CharField(source='get_status_display', read_only=True)
+
     class Meta:
         model = Compra
         fields = '__all__'
-        usuario = CharField(source='usuario.e-mail', read_only=True)
-        itens = ItensCompraSerializer(many=True, read_only=True)
-        status = CharField(source='get_status_display', read_only=True)
+       
 
